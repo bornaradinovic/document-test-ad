7a13b71a2ae635d90f2cbacde00c4bcd5b481878
Borna Radinovic <borna.radinovic@rocketinsights.com>
2023-02-08 17:33:54 +0100

    stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { DropzoneNative } from "./DropzoneNative";
 import { MyDropzone } from "./MyDropzone";
 
 function App() {
   return (
     <div>
       <MyDropzone />
+      <br />
+      <br />
+      <DropzoneNative />
     </div>
   );
 }
